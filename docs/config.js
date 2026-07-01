f22162b7087c6f879552d1ac3026744387bded58
smouyzh62-create <smouyzh62-create@users.noreply.github.com>
2026-07-02 00:36:30 +0800

    Sync local changes to GitHub

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.SITE_CONFIG = {
-  "whatsappNumber": "2453452535353252",
+  "whatsappNumber": "333333333333",
   "facebookPixelId": "22222121212",
   "whatsappMessage": "Hola, me interesa solicitar un préstamo regular sin anticipos. Mi número es {phone}."
 };
